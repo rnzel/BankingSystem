877d4754e1495b09ae340afb16e6c587a36cb5d8
rnzel <gobrisranzel@gmail.com>
2024-11-23 10:48:54 +0800

    Update SavingsAccount.java

--- javaProgram/src/com/OOP/SavingsAccount.java
+++ javaProgram/src/com/OOP/SavingsAccount.java
@@ -1,31 +1,38 @@
 package com.OOP;
 
+// The SavingsAccount class extends the Account class, representing savings account with minimum balance and interest rate
 public class SavingsAccount extends Account{
-	private double minimumBalance;
-	private double interestRate;
-	
+	private double minimumBalance; // Minimum balance in the account
+	private double interestRate; // Interest rate for the savings account
+
+	// Constructor to create a new SavingsAccount
 	public SavingsAccount(Bank bank, Customer customer, int accountNumber, double balance, double minimumBalance, double interestRate) {
-		super(customer, accountNumber, balance);
-		this.minimumBalance = minimumBalance;
-		this.interestRate = interestRate;
+		super(customer, accountNumber, balance); // Initialize the Account class
+		this.minimumBalance = minimumBalance; // Set minimum balance
+		this.interestRate = interestRate; // Set interest rate
 	}
-	
+
+	// Get minimum balance
 	public double getMinimumBalance() {
 		return minimumBalance;
 	}
-	
+
+	// Set new minimum balance
 	public void setMinimumBalance(double minimumBalance) {
 		this.minimumBalance = minimumBalance;
 	}
-	
+
+	// Get interest rate
 	public double getInterestRate() {
 		return interestRate;
 	}
-	
+
+	// Set interest rate
 	public void setInterestRate(double interestRate) {
 		this.interestRate = interestRate;
 	}
-	
+
+	// Method to calculate interest based on the balance and interest rate
 	public double calculateInterest() {
 		return getBalance() * (interestRate) / 100;
 	}
