c05c385caa2ba73245ab18eb308c5d1dceab780c
rnzel <gobrisranzel@gmail.com>
2024-11-23 09:55:47 +0800

    Update Bank.java

--- javaProgram/src/com/OOP/Bank.java
+++ javaProgram/src/com/OOP/Bank.java
@@ -2,61 +2,78 @@ package com.OOP;
 
 import java.util.ArrayList;
 
+// The Bank class represents a bank entity, managing its name, code, and lists of accounts and loans
 public class Bank{
-	private String name;
-	private int code;
-	private ArrayList<Account> accounts;
-	private ArrayList<Loan> loans;
-	
+	// Private fields to store the bank's name, code, and collections of accounts and loans
+	private String name; // Name of the bank
+	private int code; // Code for the bank
+	private ArrayList<Account> accounts; // List of accounts in the bank
+	private ArrayList<Loan> loans; // List of loans in the bank
+
+	// Constructor to create a Bank object
 	public Bank(String name, int code) {
-		this.name = name;
-		this.code = code;
-		this.accounts = new ArrayList<>();
-		this.loans = new ArrayList<>();
+		this.name = name; // Name of the bank
+		this.code = code; // Code for the bank
+		this.accounts = new ArrayList<>(); // Initialize the accounts list
+		this.loans = new ArrayList<>(); // Initialize the loans list
 	}
-	
+
+	// Get the bank's name
 	public String getName() {
 		return name;
 	}
-	
+
+	//  Set the bank's name
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	 // Get the bank's code
 	public int getCode() {
 		return code;
 	}
-	
+
+	// Set the bank's code
 	public void setCode(int code) {
 		this.code = code;
 	}
-	
+
+	// Get the list of accounts
 	public ArrayList<Account> getAccounts(){
 		return accounts;
 	}
-	
+
+	// Set a new list of accounts
 	public void setAccounts(ArrayList<Account> accounts) {
 		this.accounts = accounts;
 	}
-	
+
+	// Get the list of loans
 	public ArrayList<Loan> getLoans(){
 		return loans;
 	}
-	
+
+	// Set a new list of loans
 	public void setLoans(ArrayList<Loan> loans) {
 		this.loans = loans;
 	}
-	
+
+	// Add a new account to the bank
 	public void addAccount(Account account) {
 		accounts.add(account);
 	}
+
+	// Remove an account from the bank
 	public void removeAccount(Account account) {
 		accounts.remove(account);
 	}
-	
+
+	// Add a new loan to the bank
 	public void addLoan(Loan loan) {
 		loans.add(loan);
 	}
-	
+
+	// Remove a loan from the bank
 	public void removeLoan(Loan loan) {
 		loans.remove(loan);
 	}
